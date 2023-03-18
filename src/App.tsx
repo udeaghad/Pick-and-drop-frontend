b24742367e71957b1bb74e0e7dd5765b4f86de95
udeaghad <dozie.udeagha@gmail.com>
2023-03-18 06:06:50 +0100

    dark theme styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css';
 import Header from './components/Header'
+import { useAppSelector } from './hooks/storeHook';
 
 const App = () => {
+
+  const { darkTheme } = useAppSelector( state => state)
   return (
-    <div className="text-xl text-red-900 font-bold">
-      <Header />
+    <div className={ darkTheme ? "dark" : ""}>
+      <div className="dark:bg-gray-900 dark:text-white min-h-screen px-4 lg:px-12 pb-20">
+        <Header />
+      </div>
     </div>
   );
 }
