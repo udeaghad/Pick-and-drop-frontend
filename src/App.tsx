7af3dc6ba2f82cffa9967a3a86ecdbc53e60b3c5
udeaghad <dozie.udeagha@gmail.com>
2023-03-21 03:15:27 +0100

    Update app with fetched company data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { useEffect } from 'react';
 import './App.css';
 import Header from './components/Header'
-import { useAppSelector } from './hooks/storeHook';
+import { useAppDispatch, useAppSelector } from './hooks/storeHook';
+import { getAllCompanies } from './features/company/companySlice';
 
 const App = () => {
 
-  const { darkTheme } = useAppSelector( state => state)
+  const { darkTheme } = useAppSelector( state => state);
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(getAllCompanies())
+  },[dispatch])
   return (
     <div className={ darkTheme ? "dark" : ""}>
       <div className="dark:bg-gray-900 dark:text-white min-h-screen">
