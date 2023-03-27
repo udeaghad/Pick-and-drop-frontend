0a1a58df258443128010cc9f35c9cee83cf1687f
udeaghad <dozie.udeagha@gmail.com>
2023-03-27 19:35:07 +0100

    Create CompanyCard component

--- src/components/CompanyCard.tsx
+++ src/components/CompanyCard.tsx
@@ -1,5 +1,28 @@
-const CompanyCard = () => {
-  return (<div>Company Card</div>)
+import { FC } from 'react';
+interface ICompanyProps {
+  id: string;
+  name: string;
+  email: string;
+  phoneNumber: string;
+  city: string;
+  state: string
+  offices: {
+    _id: string;
+    name: string;
+    location: string;
+  }[];
+  rating: number;
+  isAdmin: boolean;
+  
+}
+
+const CompanyCard: FC<ICompanyProps> = (props) => {
+  const {id, name, email, phoneNumber, city, state, offices, rating, isAdmin } = props;
+
+  return (<div>
+    <div>{id}</div>
+    <div>{name}</div>
+  </div>)
 }
 
 export default CompanyCard;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,11 @@
 import { useEffect } from 'react';
-import { useAppDispatch } from '../hooks/storeHook';
+import { useAppDispatch, useAppSelector } from '../hooks/storeHook';
 import { getAllCompanies } from '../features/company/companySlice';
 import CompanyCard from '../components/CompanyCard';
 
 
 const HomePage = () => {
-
+  const { companies } = useAppSelector(state => state)
   const dispatch = useAppDispatch()
 
   useEffect(() => {
@@ -14,10 +14,26 @@ const HomePage = () => {
   
   return (
     
-      <div >
-        This is home page
+      <div >        
+       {companies.data?.map(company => {
+        const { _id, name, email, phoneNumber, city, state, offices, rating, isAdmin} = company
+        return (
 
-        <CompanyCard />
+          <CompanyCard 
+           key={_id}
+           id={_id} 
+           name={name} 
+           email={email} 
+           phoneNumber={phoneNumber} 
+           city={city}
+           state={state}
+           offices={offices}
+           rating={rating}
+           isAdmin={isAdmin}
+           />
+        )
+      }) }
+
       </div>
   );
 }
