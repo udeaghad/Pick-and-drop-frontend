422afb80766ac4825ddf1a058679736c094dfe73
udeaghad <dozie.udeagha@gmail.com>
2023-03-28 20:26:19 +0100

    Apply styling to CompnayCard.tsx

--- src/components/CompanyCard.tsx
+++ src/components/CompanyCard.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import {BsTelephonePlusFill } from 'react-icons/bs';
+import { NavLink } from 'react-router-dom';
 
 interface ICompanyProps {
   id: string;
@@ -24,30 +25,34 @@ const CompanyCard: FC<ICompanyProps> = (props) => {
   return (
     <div className="max-w-sm bg-white mx-auto rounded-lg border border-gray-200 shadow-md dark:bg-red-700 dark:border-white-200 hover:-translate-y-4 duration-200 hover:animate-pulse">
       <div className="py-5">
-        <h2 className="text-center font-black text-lg"> 
+        <h1 className="text-center font-black text-xl"> 
           {name.toUpperCase()}
-        </h2>
+        </h1>
       </div>
       
       <div>
         <img src="charlesdeluvio-pOUA8Xay514-unsplash.jpg" alt={`img-${name}`} className="aspect-[6/2] object-cover"/>
       </div>
 
-      <div>
-        <span>{city}, {state}</span>
+      <div className='py-2 border-b-2 border-gray-900 dark:border-white'>
+        <h2 className="text-center text-lg font-bold">{city}, {state}</h2>
       </div>
 
       <div>
-        <h3>Select a pick-up branch</h3>
+        <h3 className="py-2 text-center text-lg font-medium">Select a pick-up branch</h3>
       </div>
 
-      <div>
+      <ul className='p-5 ml-4 grid grid-cols-2 gap-4 list-disc'>
         {offices.map(office => (          
-            <div key={office._id}>
-              <button>{office.location}</button>
-            </div>          
+            <li key={office._id}>
+              <NavLink
+              to='#!'
+              className="hover:text-violet-400 active:text-violet-600">
+                {office.location}
+              </NavLink>
+            </li>          
         ))}
-      </div>
+      </ul>
 
       <div>
         <BsTelephonePlusFill />
