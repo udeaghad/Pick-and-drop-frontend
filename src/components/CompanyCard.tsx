0b35172b08c73cf90b82f5bec54f8cd295af68ed
udeaghad <dozie.udeagha@gmail.com>
2023-03-28 15:53:22 +0100

    Update CompanyCard.tsx with data values

--- src/components/CompanyCard.tsx
+++ src/components/CompanyCard.tsx
@@ -1,4 +1,6 @@
 import { FC } from 'react';
+import {BsTelephonePlusFill } from 'react-icons/bs'
+
 interface ICompanyProps {
   id: string;
   name: string;
@@ -17,12 +19,43 @@ interface ICompanyProps {
 }
 
 const CompanyCard: FC<ICompanyProps> = (props) => {
-  const {id, name, email, phoneNumber, city, state, offices, rating, isAdmin } = props;
+  const {name, city, state, offices, rating, phoneNumber } = props;
 
-  return (<div>
-    <div>{id}</div>
-    <div>{name}</div>
-  </div>)
+  return (
+    <div className="max-w-sm bg-white mx-auto rounded-lg border border-gray-200 shadow-md dark:bg-red-700 dark:border-white-200 hover:-translate-y-4 duration-200 hover:animate-pulse">
+      <div>
+        <h2>{name.toUpperCase()}</h2>
+      </div>
+      
+      <div>
+        <img src="charlesdeluvio-pOUA8Xay514-unsplash.jpg" alt={`img-${name}`} />
+      </div>
+
+      <div>
+        <span>{city}, {state}</span>
+      </div>
+
+      <div>
+        <h3>Select a pick-up branch</h3>
+      </div>
+
+      <div>
+        {offices.map(office => (          
+            <div key={office._id}>
+              <button>{office.location}</button>
+            </div>          
+        ))}
+      </div>
+
+      <div>
+        <BsTelephonePlusFill />
+        <span>{phoneNumber}</span>
+      </div>
+
+      <div>
+        <span>Rating: {rating}</span>
+      </div>
+    </div>)
 }
 
 export default CompanyCard;
