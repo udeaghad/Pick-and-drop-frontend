b7dcaca1b890f812fd2089463fa3242b5afd042d
udeaghad <dozie.udeagha@gmail.com>
2023-04-15 13:41:20 +0100

    Create test for SearchBar component

---
+++ src/__test__/units/SearchBar.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
+import { store } from '../../store';
+import SearchBar from '../../components/SearchBar';
+import { useState } from 'react';
+
+describe('SearchBar Component', () => {
+  const setSearchTerm = () => {
+   return "lagos"
+  }
+
+  it("Should render input field", () => {
+    render(
+      <BrowserRouter>
+          <Provider store={store}>
+          <SearchBar setSearchTerm={setSearchTerm} />
+          </Provider>
+        </BrowserRouter>
+    );
+
+    const searchField = screen.getByRole('searchbox')
+    expect(searchField).toHaveAttribute('placeholder', 'Search by city or state...')
+    
+  })
+})
